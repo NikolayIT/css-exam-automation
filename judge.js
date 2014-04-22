023e6ea33df7fa58b7793679327ddd44ff4f5c72
NikolayIT <admin@nikolay.it>
2014-04-22 11:38:56 +0300

    judge.js initial upload

---
+++ judge.js
@@ -0,0 +1,41 @@
+var page = require('webpage').create();
+var tests = require('./tests.js').tests;
+
+function equal(expected, received) {
+	return expected == received;
+};
+
+function equalDiff(expected, received, diff) {
+	return Math.abs(expected - received) <= diff;
+};
+
+function compare(functionName, expected, received, param) {
+	if (functionName == "equal")
+		return equal(expected, received);
+	if (functionName == "equalDiff")
+		return equalDiff(expected, received, param);
+};
+
+page.viewportSize = {width: 960, height: 800};
+page.open('index.html', function() {
+	var points = 0;
+	var maxPoints = 0;
+	for(var i = 0; i < tests.length; i++) {
+		test = tests[i];
+		console.log(test.name);
+		var result = page.evaluate(test.func);
+		console.log("expected: " + test.expected + "\nreceived: " + result);
+		if (compare(test.compare, test.expected, result, test.compareParam)) {
+			points += test.points;
+			console.log("Correct! +" + test.points);
+		} else {
+			console.log("Wrong");
+		}
+		maxPoints += test.points;
+		console.log("==========================");
+	};
+	
+	console.log("Total points: " + points + " / " + maxPoints);
+	
+	phantom.exit();
+});

beaf23f625c8d8b337008ad5fe9d0275af486abf
Nikolay Kostov <admin@nikolay.it>
2015-05-10 14:26:49 +0300

    Work on ChromiumHeadlessBrowserWithCefSharp

--- CefSharp/CssExamAutomation.Runner/Program.cs
+++ CefSharp/CssExamAutomation.Runner/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CssExamAutomation.Runner
+﻿namespace CssExamAutomation.Runner
 {
-    class Program
+    using System;
+    using System.IO;
+
+    using CssExamAutomation.Engine;
+
+    public static class Program
     {
-        static void Main(string[] args)
+        private static IHeadlessBrowser browser;
+
+        public static void Main(string[] args)
         {
+            browser = new ChromiumHeadlessBrowserWithCefSharp("file:///C:/Users/Nikolay/Desktop/index.html", 1024, 768);
+            browser.PageReady += BrowserPageReady;
+            
+            Console.ReadLine();
+        }
+
+        private static void BrowserPageReady(object sender, EventArgs e)
+        {
+            var screenshotPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "screenshot.png");
+            browser.SaveScreenshot(screenshotPath);
+            Console.WriteLine("Ready...");
         }
     }
 }

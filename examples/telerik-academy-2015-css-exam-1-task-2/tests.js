862a20c1ec88d048a3c32b1d7cfb0cceada042a9
Dilyan Traykov <dtraykov94@gmail.com>
2015-05-15 00:46:25 +0300

    Possible Fixes for the #header and #shellHeader Height Tests

--- examples/telerik-academy-2015-css-exam-1-task-2/tests.js
+++ examples/telerik-academy-2015-css-exam-1-task-2/tests.js
@@ -117,12 +117,16 @@ exports.tests = [
     
     // Heights (max 20)
     {name:"Header height", points: 2, func:function(){
-        return $("#header").outerHeight();
-    }, expected: 130, compare: "equalDiff", compareParam: 5},
+        return ($("#header").outerHeight() >= 125 && $("#header").outerHeight() <= 135) ||
+                ($("body").outerHeight() - $("#main").offset().top >= 125 &&
+                 $("body").outerHeight() - $("#main").offset().top <= 135)
+    }, expected: true, compare: "equal", compareParam: null},
     
     {name:"#shellHeader height", points: 2, func:function(){
-        return $("#shellHeader").outerHeight();
-    }, expected: 65, compare: "equalDiff", compareParam: 3},
+        return ($("#shellHeader").outerHeight() >= 62 && $("#shellHeader").outerHeight() <= 68) ||
+                ($("body").outerHeight() - $("#search").offset().top >= 62 &&
+                 $("body").outerHeight() - $("#search").offset().top <= 68)
+    }, expected: true, compare: "equal", compareParam: null},
     
     {name:"#search height", points: 2, func:function(){
         return $("#search").outerHeight();
